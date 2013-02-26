a4778c13517a27bac33d41100c2940d22302611a
Jose Torres <jmtorres22@gmail.com>
2013-02-26 01:08:23 -0500

    Updated address format.

--- theme/addressfield-invoice-formatter.tpl.php
+++ theme/addressfield-invoice-formatter.tpl.php
@@ -1,9 +1,11 @@
+<br/>
 <?php print implode('<br />', array_filter(array(
+    $address['name_line'],
     $address['thoroughfare'],
     $address['premise'],
     $address['sub_premise'],
     $address['dependent_locality'],
   )));
 ?>
-<br />
-<?php print $address['country'] . ' - ' . $address['postal_code'] . ' ' . $address['locality'] ?>
+<br/>
+<?php print "{$address['locality']}, {$address['administrative_area']} {$address['postal_code']}";
